6f75ad0ea10c5eaf39e33762d780ec27e08d02e6
olkadr <kadri.oluwagbemi@gmail.com>
2019-06-10 18:09:26 +0300

    Updates

---
+++ precache-manifest.10748ccdc7ea5606afab5d66e3de1984.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c903bf78e3f450af1f448a287a045c",
+    "url": "/shopping-cart-demo/index.html"
+  },
+  {
+    "revision": "c27663860c19aaeb88d1",
+    "url": "/shopping-cart-demo/static/css/main.3abc8af1.chunk.css"
+  },
+  {
+    "revision": "1936c849c19f4d1605d1",
+    "url": "/shopping-cart-demo/static/js/2.4a8c5b7d.chunk.js"
+  },
+  {
+    "revision": "c27663860c19aaeb88d1",
+    "url": "/shopping-cart-demo/static/js/main.294e3044.chunk.js"
+  },
+  {
+    "revision": "1aa8d504a5ac2a828786",
+    "url": "/shopping-cart-demo/static/js/runtime~main.e15a878c.js"
+  },
+  {
+    "revision": "65b97ec7afbe542fc1e5c93e4f822564",
+    "url": "/shopping-cart-demo/static/media/alexandra-gorn-300407.65b97ec7.jpg"
+  },
+  {
+    "revision": "f958b880994c207794899f0650317024",
+    "url": "/shopping-cart-demo/static/media/canvas-shoes-close-up.f958b880.jpg"
+  },
+  {
+    "revision": "b18e9bce1e0cdec59e65a8e754866458",
+    "url": "/shopping-cart-demo/static/media/dumbbells-shoe.b18e9bce.jpg"
+  },
+  {
+    "revision": "e84e44dce9b1ff9274212c15c9407b96",
+    "url": "/shopping-cart-demo/static/media/shoes.e84e44dc.jpg"
+  }
+]);
